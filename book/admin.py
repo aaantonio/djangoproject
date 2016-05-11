35008c658cf4dea7e8a8abddb35530c295b45cca
Arnel <Arnel@gmail.com>
2016-05-11 11:20:16 +0400

    add author admin

--- book/admin.py
+++ book/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from book.models import Publisher, Author, Book
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ['first_name', 'last_name', 'email']
+	search_fields = ['first_name', 'last_name']
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
--- book/models.py
+++ book/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name  = models.CharField(max_length=50)
-    email      = models.CharField(max_length=20)
+    email      = models.EmailField(max_length=50, verbose_name='e-mail', blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
