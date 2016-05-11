1bb96b0f50c89c1269bcf97711e986254da9ace8
Arnel <Arnel@gmail.com>
2016-05-11 12:13:13 +0400

    add ModelAdmin

--- book/admin.py
+++ book/admin.py
@@ -6,6 +6,12 @@ class AuthorAdmin(admin.ModelAdmin):
 	list_display = ['first_name', 'last_name', 'email']
 	search_fields = ['first_name', 'last_name']
 
-admin.site.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+	list_display = ['name', 'website']
+
+class BookAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Publisher, PublisherAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
