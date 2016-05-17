deb3617bf485ed4a983f06bcd620c59702b19ec0
Arnel <Arnel@gmail.com>
2016-05-17 13:30:45 +0400

    Created authorcreateview in views

--- arnel/urls.py
+++ arnel/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from views import MyView, PlusTime
+from book.views import AuthorCreateView, AuthorListView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', MyView.as_view()),
     url(r'^plus/$', PlusTime.as_view()),
     url(r'^plus/(?P<plus>[\d]+)/', PlusTime.as_view()),
+    url(r'^author/create/$', AuthorCreateView.as_view()),
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView
+from .models import Author, Publisher, Book
 
 # Create your views here.
+class AuthorCreateView(CreateView):
+	template_name = 'authorcreate.html'
+	model = Author
+	fields = ['first_name', 'last_name', 'email']
+	success_url = '/'
+
+class AuthorListView(ListView):
+	pass
