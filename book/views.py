5a0f29761471f5f1ad6503652b07c8446e55cbe7
aaantonio <arnel.aantonio@gmail.com>
2016-05-19 02:22:15 +0800

    authorcreateview

--- book/views.py
+++ book/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
 # Create your views here.
+from .models import Book, Publisher, Author
+
+class AuthorCreateView(CreateView):
+    template_name = 'authorcreateview.html'
+    model = Author
+    fields = ['first_name', 'last_name', 'email']
